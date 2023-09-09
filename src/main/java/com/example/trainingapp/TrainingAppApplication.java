2752689230f7a4aa5c6bd3ec37d0ec252f26f80f
Nicat-dev <nijatmehdiyevv@gmail.com>
2023-09-09 21:00:28 +0400

    Initial commit

---
+++ src/main/java/com/example/trainingapp/TrainingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.trainingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrainingAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/trainingapp/TrainingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
